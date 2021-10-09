986c07e10c542a54ab393fa391f96565dbdedbfb
Baron <burnsbaron@gmail.com>
2021-10-08 17:10:30 -0700

    Complete

---
+++ fancifymytext.js
@@ -0,0 +1,30 @@
+function biggerFunction(){
+	document.getElementById("mainTextArea").style.fontSize = "larger";
+}
+function deselectFunction(){
+	alert("You have deselected my button!");
+}
+function fancifyFunction(){
+	const textBox = document.getElementById("mainTextArea");
+	textBox.style.fontWeight = "bold";
+	textBox.style.color = "blue";
+	textBox.style.textDecoration = "underline";
+	//document.getElementById("BoringBetty").checked = "false";
+}
+function plainFunction(){
+	const textBox = document.getElementById("mainTextArea");
+	//textBox.style.fontStyle = "normal";
+	textBox.style.fontWeight = "normal";
+	textBox.style.color = "black";
+	textBox.style.textDecoration = "initial";
+	//document.getElementById("FancyShmancy").checked = "false";
+}
+function mooFunction(){
+	const textBox = document.getElementById("mainTextArea");
+	textBox.style.textTransform = "uppercase";
+	var str = textBox.value;
+	console.log(str);
+	var parts = str.split(".");
+	str = parts.join(". -Moo ");
+	textBox.value = str;
+}
